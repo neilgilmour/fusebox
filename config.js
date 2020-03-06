d40def8f211b2b7fcf9b4e1027a5b35206920e1d
neil <neil@motional.io>
2020-03-06 20:44:53 +0000

    Update config.js with fusebox details

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
-  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
-  siteRss: '/rss.xml',
-  siteFBAppID: '', // FB Application ID for using app insights
-  googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
-}
+  siteTitle: "FuseBox Cornwall - Truro Handyman", // Site title.
+  siteTitleAlt: "Electrical and handyman services in Cornwall", // Alternative site title for SEO.
+  siteLogo: "/icons/icon-512x512.png", // Logo used for SEO and manifest.
+  siteUrl: "https://fuseboxcornwall.netlify.com", // Domain of your website without pathPrefix.
+  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription:
+    "Fusebox Cornwall - Electrical and handyman services in Truro and Cornwall", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml",
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleTagManagerID: "", // GTM tracking ID.
+  //disqusShortname: 'gatsby-business-starter', // Disqus shortname.
+  userName: "Vaibhav Sharma",
+  userTwitter: "vaibhaved",
+  userLocation: "Delhi NCR, India",
+  userDescription: "",
+  copyright: "Copyright © FuseBox Cornwall 2020. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#b68b16", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff" // Used for setting manifest background color.
+};
